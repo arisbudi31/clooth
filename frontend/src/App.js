182827ddfab3656a86fc14ad644dfa8d71ce1761
arisbudi31 <arisjoharie@gmail.com>
2022-07-21 05:43:05 +0700

    resolve conflict app.js on main
    -hapus use effect yg tanpa pake api_url
    - combine route page user dengan admin

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 import React, { useEffect } from "react";
-import { useDispatch} from "react-redux";
+import { useDispatch } from "react-redux";
 import { Routes, Route } from 'react-router-dom';
 import DetailProduct from './pages/admin/products/DetailProduct';
 import FormProduct from './pages/admin/products/FormProduct';
@@ -11,17 +11,13 @@ import Login from './pages/admin/login';
 import ResetPassword from './pages/admin/reset';
 import HomeAdmin from './pages/admin/home-admin';
 import ManageUsers from "./pages/admin/Manage/manage-users";
-<<<<<<< Updated upstream
-import { GET_ADMIN_DATA } from './redux/actions/types'
 import Home from "./pages/user/Home";
 import Products from "./pages/user/Products";
 import DetailProductUser from "./pages/user/DetailProductUser";
-=======
 import AddAdmin from "./pages/admin/Manage/add-admin";
 import NewOrder from "./pages/admin/orders/new-order";
 import AllOrders from "./pages/admin/orders/all-orders";
-import {GET_ADMIN_DATA, LOADING_END, LOADING_START} from './redux/actions/types'
->>>>>>> Stashed changes
+import { GET_ADMIN_DATA, LOADING_END, LOADING_START } from './redux/actions/types'
 
 
 function App() {
@@ -29,18 +25,19 @@ function App() {
   const dispatch = useDispatch()
   const token = localStorage.getItem("tokenAdmin")
 
-<<<<<<< Updated upstream
   useEffect(() => {
-    axios.get('http://localhost:5000/api/admin/keep-login',
-      { headers: { "authToken": token } })
+    dispatch({ type: LOADING_START })
+    axios.get(API_URL + '/admin/keep-login', { headers: { "authToken": token } })
       .then((resp) => {
-        console.log(`respond when keep login:`, resp.data);
+        dispatch({ type: LOADING_END })
         dispatch({ type: GET_ADMIN_DATA, payload: resp.data })
+
       })
       .catch((err) => {
+        dispatch({ type: LOADING_END })
         console.log(`error when keep login:`, err);
       })
-  }, [])
+  }, [0])
 
   return (
     <div>
@@ -50,7 +47,10 @@ function App() {
         <Route path="/detail-product/:id" element={<DetailProductUser />} />
         <Route path="/admin" element={<HomeAdmin />} />
         <Route path="/admin/login" element={<Login />} />
-        <Route path="/admin/manage-users" element={<ManageUsers />} />
+        <Route path="/admin/add-admin" element={<AddAdmin />} />
+        <Route path="/admin/users-list" element={<ManageUsers />} />
+        <Route path="/admin/orders/new-order" element={<NewOrder />} />
+        <Route path="/admin/orders/all-orders" element={<AllOrders />} />
         <Route path="/admin/reset/:adminname/reset/:emailAdmin" element={<ResetPassword />} />
         <Route path="/admin/product" element={<Product />} />
         <Route path="/admin/edit-product/:id" element={<FormProduct />} />
@@ -60,41 +60,7 @@ function App() {
         <Route path="/admin/add-category/" element={<FormCategories />} />
       </Routes>
     </div>
-=======
-  useEffect(()=> {
-    dispatch({type: LOADING_START})
-    axios.get(API_URL + '/admin/keep-login', { headers: {"authToken": token}})
-    .then((resp) => {
-      dispatch({type: LOADING_END})
-      dispatch({type: GET_ADMIN_DATA, payload: resp.data})
 
-    })
-    .catch((err) => {
-      dispatch({type: LOADING_END})
-      console.log(`error when keep login:`, err);
-    })
-  }, [0])
-
-  return (
-      <div>
-        <Routes>
-          <Route path="/admin" element={<HomeAdmin/>}/>
-          <Route path="/admin/login" element={<Login/>}/>
-          <Route path="/admin/add-admin" element={<AddAdmin/>}/>
-          <Route path="/admin/users-list" element={<ManageUsers/>}/>
-          <Route path="/admin/orders/new-order" element={<NewOrder/>}/>
-          <Route path="/admin/orders/all-orders" element={<AllOrders/>}/>
-          <Route path="/admin/reset/:adminname/reset/:emailAdmin" element={<ResetPassword/>}/>
-          <Route path="/admin/product" element={<Product />} />
-          <Route path="/admin/edit-product/:id" element={<FormProduct />} />
-          <Route path="/admin/add-product" element={<FormProduct />} />
-          <Route path="/admin/detail-product/:id" element={<DetailProduct />} />
-          <Route path="/admin/categories/" element={<Categories />} />
-          <Route path="/admin/add-category/" element={<FormCategories />} />
-        </Routes>
-
-      </div>
->>>>>>> Stashed changes
   );
 }
 
--- frontend/src/component/Header.jsx
+++ frontend/src/component/Header.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import Sidebar from "./Sidebar";
-import {Text} from '@chakra-ui/react'
+import { Text } from '@chakra-ui/react'
 import { useNavigate } from 'react-router-dom'
 import { useDispatch, useSelector } from "react-redux";
 
@@ -14,10 +14,6 @@ function Header() {
   }
 
   const admin = useSelector((state) => state.admin)
-<<<<<<< Updated upstream
-  // console.log(`data admin:`, admin);
-=======
->>>>>>> Stashed changes
 
   return (
     <>
